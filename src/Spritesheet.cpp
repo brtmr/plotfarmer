a12e3d7bafe727668326e833dd43e4a9991bc026
plotfarmer <Reitemei@students.uni-marburg.de>
2014-02-17 15:43:51 +0100

    added Spritesheet managing

---
+++ src/Spritesheet.cpp
@@ -0,0 +1,51 @@
+#include"Spritesheet.h"
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<vector>
+#include<iostream>
+
+/*
+ * path is the filename of the spritesheet
+ * m is the number of rows
+ * n is the number of columns
+ * p ist the total number of sprites in the sheet
+ */
+
+Spritesheet::Spritesheet(std::string path, int m, int n, int p, SDL_Renderer* gameRenderer)
+{
+    SDL_Surface* spritesSurface = IMG_Load(path.c_str());
+    sprites = SDL_CreateTextureFromSurface(
+        gameRenderer,
+        spritesSurface
+    );
+    for(int i=0; i<p; i++){
+        clipRectangles.push_back(createClipRectangle(spritesSurface, m, n, i));
+        }
+    singleWidth  = (spritesSurface->w)/n;
+    singleHeight = (spritesSurface->h)/m;
+    //SDL_FreeSurface(spritesSurface);
+}
+
+Spritesheet::~Spritesheet()
+{
+    SDL_DestroyTexture(sprites);
+}
+
+
+/* 
+ * This functionn returns the clipRectangle for a for the
+ * ith sprite (zero-based) in a mxn spritesheet
+ * where m is the number of rows
+ * where n is the number of columns
+ * */
+SDL_Rect Spritesheet::createClipRectangle(SDL_Surface* spritesheet, int m, int n, int i)
+{
+    int w = (spritesheet->w)/n;
+    int h = (spritesheet->h)/m;
+    SDL_Rect result;
+    result.w = w;
+    result.h = h;
+    result.x = (i%n)*w;
+    result.y = (i/n)*h;
+    return result;
+}
---
+++ src/Spritesheet.h
@@ -0,0 +1,21 @@
+#ifndef SPRITESHEET_H
+#define SPRITESHEET_H
+
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<vector>
+#include<iostream>
+
+class Spritesheet
+{
+    public:
+        Spritesheet(std::string path, int m, int n, int p, SDL_Renderer* gameRenderer);
+        ~Spritesheet();
+        SDL_Rect createClipRectangle(SDL_Surface*,int,int,int);
+        SDL_Texture* sprites;
+        std::vector<SDL_Rect> clipRectangles;
+        int singleWidth;
+        int singleHeight;
+};
+
+#endif
